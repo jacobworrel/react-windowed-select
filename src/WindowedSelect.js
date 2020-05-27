9e85577a5aada911401fdf0f674815bac08477a8
Jake Chamberlain <jake@jchamb.com>
2020-05-27 12:28:44 -0400

    add sizing

--- src/WindowedSelect.js
+++ src/WindowedSelect.js
@@ -1,8 +1,7 @@
 import MenuList from './MenuList';
-import React from 'react';
+import React, { useMemo } from 'react';
 import Select from 'react-select';
 import { calcOptionsLength } from './util';
-import { useMemo } from 'react';
 
 function WindowedSelect (props, ref) {
   const { windowThreshold = 100 } = props;
