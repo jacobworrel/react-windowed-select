27e07d770665f351f312f7cb1f203241dbfbc486
Jacob Worrel <jacobworrel@gmail.com>
2024-03-05 21:02:17 -0800

    Revert "Adds ES build together with CJS"

--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-    entry: ['src/index.ts'],
-    sourcemap: true,
-    clean: true,
-    dts: true,
-    format: ['esm', 'cjs']
-});
